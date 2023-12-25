4eae579c53e37cc07f2b622a08a7666f9c5eeae5
grgtr <sakharov.da@phystech.edu>
2023-12-25 15:08:17 +0300

    merged from client-observability

--- client-cvc/go.mod
+++ client-cvc/go.mod
@@ -9,6 +9,8 @@ require (
 )
 
 require (
+	github.com/go-logr/logr v1.3.0 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
@@ -19,6 +21,11 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.opentelemetry.io/otel v1.21.0 // indirect
+	go.opentelemetry.io/otel/metric v1.21.0 // indirect
+	go.opentelemetry.io/otel/trace v1.21.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
--- client-cvc/internal/app/app_client.go
+++ client-cvc/internal/app/app_client.go
@@ -3,6 +3,9 @@ package app
 
 import (
 	"context"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/trace"
+	"go.uber.org/zap"
 	"log"
 	"net/http"
 	"os"
@@ -18,24 +21,43 @@ type App struct {
 	Config   *config.Config
 	Server   *http.Server
 	Database *mongodb.Database
+	Logger   *zap.SugaredLogger
+	Tracer   trace.Tracer
 }
 
 // NewApp initializes and returns a new instance of the client service application.
 func NewApp(cfg *config.Config) *App {
+	// Создание логгера
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Fatalf("Logger init error. %v", err)
+	}
+	sugLog := logger.Sugar()
+	sugLog.Info("Logger initialized!")
+
+	// Создание трейсера
+	tracer := otel.Tracer("final")
+	sugLog.Info("Tracer created")
+
+	// Установка подключения к БД
 	db, err := mongodb.NewDatabase(cfg.Database.URI, cfg.Database.Name, "root", "example") // Pass the database name from config
 	if err != nil {
 		log.Fatal("Error initializing database:", err)
 	}
+	sugLog.Info("Connection established successfully!")
 
+	sugLog.Info("Creating server")
 	server := &http.Server{
 		Addr:    cfg.HTTP,
 		Handler: nil, // Use nil handler initially
 	}
-
+	sugLog.Info("Server created successfully!")
 	return &App{
 		Config:   cfg,
 		Server:   server,
 		Database: db,
+		Logger:   sugLog,
+		Tracer:   tracer,
 	}
 }
 
@@ -47,6 +69,7 @@ func (app *App) Start() {
 	go func() {
 		log.Println("Server is starting on", app.Config.HTTP)
 		if err := app.Server.ListenAndServe(); err != nil {
+			app.Logger.Fatalf("Server error. %v", err)
 			log.Fatal("Error starting server:", err)
 		}
 	}()
@@ -56,6 +79,7 @@ func (app *App) Start() {
 	signal.Notify(quit, os.Interrupt)
 
 	<-quit
+	app.Logger.Info("Shutting down the server...")
 	log.Println("Shutting down the server...")
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -66,6 +90,5 @@ func (app *App) Start() {
 	if err := app.Server.Shutdown(ctx); err != nil {
 		log.Fatal("Error shutting down server:", err)
 	}
-
-	log.Println("Server has stopped.")
+	app.Logger.Info("Server has stopped successfully.")
 }
